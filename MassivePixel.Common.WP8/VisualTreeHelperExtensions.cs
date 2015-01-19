69ea706ea1c5066490cf714a7dee859bcb710f13
Toni Petrina <tonipetrina@hotmail.com>
2015-01-19 19:18:23 +0100

    Initial project added with extensions for VisualTreeHelper

---
+++ MassivePixel.Common.WP8/VisualTreeHelperExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
+
+namespace MassivePixel.Common.WP8
+{
+    public static class VisualTreeHelperExtensions
+    {
+        public static IEnumerable<T> GetChildren<T>(this DependencyObject o)
+            where T : DependencyObject
+        {
+            if (o == null)
+                throw new ArgumentNullException("o");
+
+            for (int index = 0, total = VisualTreeHelper.GetChildrenCount(o);
+                index < total;
+                index++)
+            {
+                var child = VisualTreeHelper.GetChild(o, index);
+                if (child is T)
+                    yield return (T)child;
+
+                foreach (var grandchild in child.GetChildren<T>())
+                    yield return grandchild;
+            }
+        }
+    }
+}
